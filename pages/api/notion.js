ace42ff370cf22d908e8fe55eb9419bbcd4ba994
Eduard Constantin <ibinceanu.eduard@yahoo.com>
2022-09-14 19:38:27 +0300

    Retrieve the token from query

--- pages/api/notion.js
+++ pages/api/notion.js
@@ -2,7 +2,7 @@ import { Client } from '@notionhq/client';
 
 export default async function handler(req, res) {
 	const notion = new Client({
-		auth: process.env.NOTION_TOKEN,
+		auth: req.query.NOTION_TOKEN,
 	});
 	const database = await notion.databases.query({
 		database_id: process.env.NOTION_DB,
